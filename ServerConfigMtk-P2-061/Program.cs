4d393bce4451d61ebe9fc63fa38a8fb7fc960c36
Bagus Rinu Pangayom <bagusrp2001@gmail.com>
2021-11-01 13:36:36 +0700

    Add Files

---
+++ ServerConfigMtk-P2-061/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceMtk_P1_061;
+
+//Nama  : Bagus Rinu Pangayom
+//NIM   : 20190140061
+//Kelas : C
+
+namespace ServerConfigMtk_P2_061
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server is ready!!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
